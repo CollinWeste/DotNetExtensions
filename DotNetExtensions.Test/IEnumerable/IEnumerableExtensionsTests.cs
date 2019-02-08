ee3bb7856c9d13d158aa698b2f8cd852c08bbddc
Collin Westenhaver <collin.westenhaver@icloud.com>
2019-02-07 22:57:36 -0800

    Adds RemoveFromEnd and tests

--- DotNetExtensions.Test/IEnumerable/IEnumerableExtensionsTests.cs
+++ DotNetExtensions.Test/IEnumerable/IEnumerableExtensionsTests.cs
@@ -3,6 +3,7 @@ using DotNetExtensions.IEnumerable;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DotNetExtensions.Test.IEnumerable
 {
@@ -42,6 +43,98 @@ namespace DotNetExtensions.Test.IEnumerable
 
       // Act - Assert
       Assert.DoesNotThrow(() => uut.ForEach(action));
+    }
+
+    #endregion
+
+    #region RemoveFromEnd
+
+    [Test]
+    public void RemoveFromEnd_NullSource_ThrowsException()
+    {
+      // Arrange
+      IEnumerable<object> uut = null;
+      int count = 0;
+
+      // Act - Assert
+      Assert.Throws<ArgumentNullException>(() => uut.RemoveFromEnd(count));
+    }
+
+    [Test]
+    public void RemoveFromEnd_NegativeCount_ThrowsException()
+    {
+      // Arrange
+      IEnumerable<object> uut = new List<object>();
+      int count = -1;
+
+      // Act - Assert
+      Assert.Throws<ArgumentOutOfRangeException>(() => uut.RemoveFromEnd(count));
+    }
+
+    [Test]
+    public void RemoveFromEnd_ValidArgs_DoesNotThrow()
+    {
+      // Arrange
+      IEnumerable<object> uut = new List<object>();
+      int count = 0;
+
+      // Act - Assert
+      Assert.DoesNotThrow(() => uut.RemoveFromEnd(count));
+    }
+
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(3)]
+    [TestCase(4)]
+    public void RemoveFromEnd_CountLessThanEqualToLength_RemovesFromEnd(int toRemove)
+    {
+      // Arrange
+      IEnumerable<object> uut = new List<object>()
+      {
+        "1",
+        "2",
+        "3",
+        "4"
+      };
+
+      var expected = uut.Reverse().Skip(toRemove).Reverse();
+      var expectedLength = expected.Count();
+
+      Assume.That(expectedLength, Is.Not.Negative);
+
+      // Act
+      uut = uut.RemoveFromEnd(toRemove);
+
+      // Assert
+      Assert.That(uut.Count(), Is.EqualTo(expectedLength));
+
+      for (var i = 0; i < uut.Count(); i ++)
+      {
+        Assert.That(uut.ElementAt(i), Is.EqualTo(expected.ElementAt(i)));
+      }
+    }
+
+    [TestCase(5)]
+    [TestCase(15)]
+    [TestCase(150)]
+    public void RemoveFromEnd_CountGreaterThanLength_RemovesAll(int toRemove)
+    {
+      // Arrange
+      IEnumerable<object> uut = new List<object>()
+      {
+        "1",
+        "2",
+        "3",
+        "4"
+      };
+
+      Assume.That(uut.Count(), Is.LessThan(toRemove));
+
+      // Act
+      uut = uut.RemoveFromEnd(toRemove);
+
+      // Assert
+      Assert.That(uut, Is.Empty);
     }
 
     #endregion
--- DotNetExtensions/IEnumerable/IEnumerableExtensions.cs
+++ DotNetExtensions/IEnumerable/IEnumerableExtensions.cs
@@ -55,5 +55,30 @@ namespace DotNetExtensions.IEnumerable
     {
       return source?.IsEmpty() ?? true;
     }
+
+    /// <summary>
+    /// Removes the specified number of elements from the end of the enumerable.
+    /// </summary>
+    /// <typeparam name="T">The source type.</typeparam>
+    /// <param name="source">The source enumerable.</param>
+    /// <param name="count">The number of elements to remove.</param>
+    /// <returns>
+    /// The enumerable with <paramref name="count"/> elements 
+    /// removed from the end. If <paramref name="count"/> is larger
+    /// than the number of elements in <paramref name="source"/>, an empty
+    /// enumerable is returned.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="source"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="count"/> is less than zero.</exception>
+    public static IEnumerable<T> RemoveFromEnd<T>(this IEnumerable<T> source, int count)
+    {
+      if (source == null) { throw new ArgumentNullException(nameof(source)); }
+      if (count < 0) { throw new ArgumentOutOfRangeException(nameof(count)); }
+
+      var newLength = source.Count() - count;
+      if (newLength < 0) { newLength = 0; }
+
+      return source.Take(newLength);
+    }
   }
 }
